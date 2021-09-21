2effdd83bf93269698c3637be59f3b5e01906be7
William Santos <me@wsantos.dev>
2021-09-21 13:30:45 -0300

    Create MessagePackLZ4SmallObject.cs
    Adding MessagePack benchmark with LZ4 compression enabled.

---
+++ DotNetSerializationBenchmark/MessagePackLZ4SmallObject.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using MessagePack;
+
+namespace DotNetSerializationBenchmark
+{
+    [MemoryDiagnoser()]
+    public class MessagePackLZ4SmallObject : Serializer<SmallObject, byte[]>
+    {
+        private readonly MessagePackSerializerOptions options = MessagePackSerializerOptions.Standard
+                                                                                            .WithCompression(MessagePackCompression.Lz4BlockArray);
+        
+        [Benchmark]
+        public override byte[] Serialize()
+        {
+            return MessagePackSerializer.Serialize(GetCachedInstance(), options);
+        }
+
+        [Benchmark]
+        public override SmallObject Deserialize()
+        {
+            return MessagePackSerializer.Deserialize<SmallObject>(GetCachedSerializedInstance(), options);
+        }
+    }
+}
